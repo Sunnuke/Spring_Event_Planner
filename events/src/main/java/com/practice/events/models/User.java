e2374ce8b27fb4ac5f1374e3a64dbae3719a975d
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-17 07:42:57 -0800

    Created overall layout of (Model) Users class

---
+++ events/src/main/java/com/practice/events/models/User.java
@@ -0,0 +1,172 @@
+package com.practice.events.models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.Transient;
+
+@Entity
+@Table(name="users")
+public class User {
+    
+//	ATTRIBUTES
+	
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    
+    @Size(min=2, max=20, message="First Name must be at least 2 characters!")
+    private String firstName;
+    
+    @Size(min=2, max=20, message="Last Name must be at least 2 characters!")
+    private String lastName;
+    
+    @Size(min=5, max=20, message="Location Name must be at least 5 characters!")
+    private String location;
+    
+    @Size(min=2, max=2, message="State must be abbreviated and 2 characters!")
+    private String state;
+    
+	@Email(message="Email must be valid!")
+    private String email;
+    
+    @Size(min=5, message="Password must be greater than 5 characters!")
+    private String password;
+    
+    @Transient
+    private String passwordConfirmation;
+    
+    @Column(updatable=false)
+    private Date createdAt;
+    private Date updatedAt;
+    
+    @OneToMany(mappedBy="user", fetch = FetchType.LAZY)
+    private List<Post> post;
+    
+    @OneToMany(mappedBy="user", fetch = FetchType.LAZY)
+    private List<Comment> comment;
+    
+    
+//	Constructors
+    
+    public User() {
+    }
+    
+//	PRESETS
+    
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+
+//	METHODS
+    
+	// Id
+    public Long getId() {
+		return id;
+	}
+
+    //  First Name
+    public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	// Last Name
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	// Location
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	// State
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+    
+    // Email
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	// Password
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	// Password Confirmation
+	public String getPasswordConfirmation() {
+		return passwordConfirmation;
+	}
+	public void setPasswordConfirmation(String passwordConfirmation) {
+		this.passwordConfirmation = passwordConfirmation;
+	}
+
+	// Date/Time
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+// 	Relationship
+  // One to Many
+	// Post
+	public List<Post> getPost() {
+		return post;
+	}
+	public void setPost(List<Post> post) {
+		this.post = post;
+	}
+	// Comments
+	public List<Comment> getComment() {
+		return comment;
+	}
+	public void setComment(List<Comment> comment) {
+		this.comment = comment;
+	}
+
+	
+}
+
